39ffb8ecdda2d76e6a1511756f56d2bec89e5f8e
jborruel308@me.com <jborruel308@me.com>
2024-11-18 19:19:16 -0800

    create index for hoots

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -26,4 +26,17 @@ router.post('/', async (req, res) => {
 });
 
 
+router.get('/', async (req, res) => {
+    try {
+
+        const hoots = await Hoot.find({})
+        .populate('author')
+        .sort({createdAt: 'desc'});
+        res.status(200).json(hoots);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+router.get('/:hootId', async (req, res) => {})
 module.exports = router;
